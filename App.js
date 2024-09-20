18b5a0b7219f55406c0a67d6f8ada4365e6cfade
Nishchalv2 <nishchalck3825@gmail.com>
2024-09-20 18:46:12 +0530

    lecture 2 : Igniting our app

--- App.js
+++ App.js
@@ -14,10 +14,13 @@
  * ReactElement(Object) => HTML(Browser Understands)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h2", {}, "This is react lessons"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
